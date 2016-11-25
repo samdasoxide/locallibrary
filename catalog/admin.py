f99c4cb3dee905ee0f00877038cedf47667d04a5
Samuel Ramdas <samdasoxide@gmail.com>
2016-11-25 17:14:02 +0300

    initial commit

---
+++ catalog/admin.py
@@ -0,0 +1,61 @@
+from django.contrib import admin
+
+from .models import Author, Book, BookInstance, Genre, Language
+
+# Register your models here.
+# Defining ModelAdmin class
+# This is to change how a model is displayed in the admin interface.
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name',
+                    'first_name',
+                    'date_of_birth',
+                    'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+class BookInstanceInline(admin.TabularInline):
+    """
+    Adding associated records at the same time
+    i.e. have book information and information about specific copies
+    on the same page.
+    """
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """
+    Getting the genere may not be a good idea
+    because of the 'cost' of the database operation.
+    We will use a function in our (Book) model to get this
+    """
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+# Register admin with the assosiated model
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+admin.site.register(Language)
---
+++ catalog/models.py
@@ -0,0 +1,153 @@
+from __future__ import unicode_literals
+
+# Required for unique book instances
+import uuid
+
+from django.db import models
+# Used to generate URLs by reverseing the URL patterns
+from django.urls import reverse
+
+
+# Create your models here.
+class Genre(models.Model):
+    """
+    Model representing a book genre (e.g. Science Fiction, Non Fiction).
+    """
+    name = models.CharField(
+        max_length=200,
+        help_text="""Enter a book genre
+                  (e.g. Science Fiction, French Poetry etc.)""")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
+
+
+class Language(models.Model):
+    """
+    Model representing the book language
+    """
+    name = models.CharField(
+        max_length=100,
+        help_text='Enter language book is written in')
+
+    def __str__(self):
+        """
+        String representation of an object
+        """
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Model representing a book (but not a specific copy of a book)
+    """
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    # Foreing key used because book can only have one author, but authors can have mutilple books
+    # Author as a string rather than object because it hasn't been declared yet in the file.
+
+    summary = models.TextField(
+        max_length=1000,
+        help_text='Enter brief description of the book')
+
+    isbn = models.CharField(
+        'ISBN',
+        max_length=13,
+        help_text='''13 Character
+        <a href="https://www.isbn-international.org/content/what-isbn">
+            ISBN number
+        </a>''')
+
+    genre = models.ManyToManyField(
+        Genre,
+        help_text='Select a genere for this book')
+
+    # ManyToManyField used because genere can contain many books. Books can cover many generes.
+    # Generes class has already been defined so we can specify the object above.
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular book instance
+        """
+        return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """
+        Creates a string for the Genre.
+        This is required to display genre in Admin.
+        """
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+
+class BookInstance(models.Model):
+    """
+    Model representing a specific copy of a book
+    (i.e. that can be borrowed from the library)
+    """
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text='Unique id for this particular book across whole library')
+
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+
+    language = models.ForeignKey(
+        'Language',
+        on_delete=models.SET_NULL,
+        null=True)
+
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('d', 'Maintanace'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True, null=True)
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        '''
+        String for representing the model object
+        '''
+        return '%s (%s)' % (self.id, self.book.title)
+
+
+class Author(models.Model):
+    """
+    Model representing an author.
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular author instance
+        """
+        return reverse('author-detail', arg=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+
+        return '%s, %s' % (self.last_name, self.first_name)
