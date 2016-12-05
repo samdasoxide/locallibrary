7ebf9a36599632f92708bb36bafe6fbab626f91b
samdasoxide <samdasoxide@gmail.com>
2016-12-05 17:11:44 +0300

    Adding book list and detail view, urls, templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^books/(?P<pk>\d+)$', views.BookDetailView.as_view(),
+        name='book-detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
 
@@ -37,3 +38,12 @@ def index(request):
                  'num_python': num_python
                  }
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
