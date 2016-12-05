68cce8d1fd352c4d00b5523138e3baf266c84d34
samdasoxide <samdasoxide@gmail.com>
2016-12-05 13:08:02 +0300

    Adding templates, views, urls and static content to catalog app

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
+from .models import Book, Author, BookInstance, Genre
+
 # Create your views here.
+
+
+def index(request):
+    """
+    view function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    # Number of all available copies of all books
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books i.e. books with status available
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The all() is implmeneted by default.
+    num_authors = Author.objects.count()
+
+    # Genre count
+    num_genre = Genre.objects.count()
+
+    # Book titles that contain the word 'python'
+    num_python = Book.objects.filter(title__icontains='python').count()
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available,
+                 'num_authors': num_authors,
+                 'num_genre': num_genre,
+                 'num_python': num_python
+                 }
+    )
