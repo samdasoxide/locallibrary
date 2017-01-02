3c782023207fe2dee1ef2ae3c25e665f5c2cb394
samdasoxide <samdasoxide@gmail.com>
2017-01-02 15:34:59 +0300

    Updating the site with site and development allowed hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRECT_KEY', '3d5*q=7(nvs%jjp-(4p=lu80vkey$
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mozila-local-library.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
